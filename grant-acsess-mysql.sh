ceda301ed13ae04d9e4cd9ebe2a269327dd521ab
denzfarid <denz.farid04@gmail.com>
2013-12-13 14:38:31 +0700

    grant acsess mysql database with enter your passwd

--- grant-acsess-mysql.sh
+++ grant-acsess-mysql.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 #denfarid
+#enter your passwd mysql
   
 EXPECTED_ARGS=4
 E_BADARGS=65
 MYSQL=`which mysql`
-  
-Q1="grant all on $1.* to $2@'$3' IDENTIFIED BY '$4';"
-Q2="show grants for $2@'$3';"
-SQL="${Q1}${Q2}"
+
+Q1="create database $1;"
+Q2="grant all on $1.* to $2@'$3' IDENTIFIED BY '$4';"
+Q3="show grants for $2@'$3';"
+PASS="YOUR_PASSWD"
+
+SQL="${Q1}${Q2}${Q3}"
   
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
+clear
   echo "Usage: $0 dbname dbuser ipaddress dbpass"
   echo "egg: $0 blog root 127.0.0.7 1234"
   exit $E_BADARGS
 fi
-$MYSQL -uroot  -e "$SQL"
+
+$MYSQL -uroot -p -e "$SQL"
 echo "Successfull!"
